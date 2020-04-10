c5f338e941d516cafa54d54644693fe3c67f35d0
Oluwadolamipo <54285015+Oluwadolamipo@users.noreply.github.com>
2020-04-10 23:45:50 +0100

    Commit

---
+++ Task2SimpleProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2SimpleProject
+{
+    class Program
+    {        static void Main(string[] args)
+        {
+           string personName = "";
+           Console.Write("Please enter your name: ");
+        personName = Console.ReadLine();
+
+        Console.WriteLine(" Welcome to RS drinks Solution " + personName);
+       
+
+        { 
+            int[] code = new int[5];
+
+         Code[0] = 10000;
+         accountBallances[1] = 20000;
+         accountBallances[2] = 5000;
+         accountBallances[3] = 100000;
+         accountBallances[4] = 9000000;
+
+        //To conclude accountBallances Console.WriteLine(accountBallances[5]
+            }
+
+        }
+    }
+}
